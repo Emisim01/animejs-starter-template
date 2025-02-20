89898269ce19928b63bd47fc3450cae1b7f25804
Emisim01 <emisim01@easv365.dk>
2025-02-20 10:38:57 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let menuIcon = document.querySelector('.menu-icon');
     let menu = document.querySelector('.menu');
+    let menuLines = document.querySelectorAll('#menu-lines path')
 
     menuIcon.addEventListener('click', () => {
         let isMenuVisible = menu.style.right === '0px';
@@ -37,6 +38,66 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         }
 
+    })
+    //part 2
+    menuIcon.addEventListener('mouseenter', () => {
+        anime({
+            targets: menuLines[1], 
+            opacity: 0,
+            duration: 200,
+            easing: 'easeInOutQuad'
+        })
+
+        anime({
+            targets: menuLines[0], 
+            translateY: 0,
+            translateX: 15,
+            rotate: 45,
+            duration: 200,
+            easing: 'easeInOutQuad',
+            strokeWidth: 3,
+        })
+
+        anime({
+            targets: menuLines[2], 
+            translateY: 15,
+            translateX: -20,
+            rotate: -45,
+            duration: 200,
+            easing: 'easeInOutQuad',
+            strokeWidth: 3,
+        })
+    })
+
+    menuIcon.addEventListener('mouseleave', () => {
+        anime({
+            targets: menuLines[1], 
+            opacity: 1,
+            duration: 200,
+            easing: 'easeInOutQuad'
+        })
+
+        anime({
+            targets: menuLines[0], 
+            translateY: 0,
+            translateX: 0,
+            rotate: 0,
+            duration: 200,
+            easing: 'easeInOutQuad',
+            strokeWidth: 1,
+
+        })
+
+        anime({
+            targets: menuLines[2], 
+            translateY: 0,
+            translateX: 0,
+            rotate: 0,
+            duration: 200,
+            easing: 'easeInOutQuad',
+            strokeWidth: 1,
+
+        })
     })
 
 })
